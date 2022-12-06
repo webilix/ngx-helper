c4475b39b16978015eea51c5949128de264c513c
Ali Amirnezhad <webilix@gmail.com>
2022-12-06 07:41:23 +0330

    Update file-size pipe

--- ngx-utils/src/lib/pipes/ngx-utils-file-size.pipe.ts
+++ ngx-utils/src/lib/pipes/ngx-utils-file-size.pipe.ts
@@ -5,21 +5,24 @@ import { Validator } from '@webilix/validator-library';
 
 @Pipe({ name: 'ngxUtilsFileSize' })
 export class NgxUtilsFileSizePipe implements PipeTransform {
-    transform(size: number): string {
+    transform(size: number, english: boolean = false): string {
         if (!Validator.VALUE.isNumber(size) || size < 0) return '';
 
-        if (size < 1000) return size.toString() + ' بایت';
+        if (size < 1000) return `${size.toString()} ${english ? 'B' : 'بایت'}`;
 
         size /= 1024;
-        if (size < 1000) return size.toFixed(2) + ' کیلوبایت';
+        if (size < 1000) return `${size.toFixed(2)} ${english ? 'KB' : 'کیلوبایت'}`;
 
         size /= 1024;
-        if (size < 1000) return size.toFixed(2) + ' مگابایت';
+        if (size < 1000) return `${size.toFixed(2)} ${english ? 'MB' : 'مگابایت'}`;
 
         size /= 1024;
-        if (size < 1000) return size.toFixed(2) + ' گیگابایت';
+        if (size < 1000) return `${size.toFixed(2)} ${english ? 'GB' : 'گیگابایت'}`;
 
         size /= 1024;
-        return Helper.NUMBER.format(+size.toFixed(2)) + ' ترابایت';
+        return (
+            Helper.NUMBER.format(+size.toFixed(2), english ? 'EN' : 'FA').replace(/,/g, english ? ',' : '،') +
+            (english ? ' TB' : ' ترابایت')
+        );
     }
 }
