f389c4d9212c5cddccd9951fb23ec7a77393cd45
Ali Amirnezhad <webilix@gmail.com>
2022-12-06 07:17:53 +0330

    Update multi-line pipe

--- ngx-utils/src/lib/pipes/ngx-utils-multi-line.pipe.ts
+++ ngx-utils/src/lib/pipes/ngx-utils-multi-line.pipe.ts
@@ -1,14 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
+import { Helper } from '@webilix/helper-library';
 import { Validator } from '@webilix/validator-library';
 
 @Pipe({ name: 'ngxUtilsMultiLine' })
 export class NgxUtilsMultiLinePipe implements PipeTransform {
     constructor(private readonly sanitizer: DomSanitizer) {}
 
-    transform(value: string): SafeHtml {
+    transform(value: string, html: boolean = false): string | SafeHtml {
         if (!Validator.VALUE.isString(value) || value === '') return '';
-        return this.sanitizer.bypassSecurityTrustHtml(value.replace(/(?:\r\n|\r|\n)/g, '<br />'));
+
+        return html
+            ? this.sanitizer.bypassSecurityTrustHtml(value.replace(/(?:\r\n|\r|\n)/g, '<br />'))
+            : Helper.STRING.escapeHTML(value).replace(/(?:\r\n|\r|\n)/g, '<br />');
     }
 }
--- testing/src/app/pipe/pipe.component.ts
+++ testing/src/app/pipe/pipe.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
     styleUrls: ['./pipe.component.scss'],
 })
 export class PipeComponent {
-    public html: string = '<a href="https://google.com" target="_blank">GOOGLE</a><br>2ND line of HTML code!';
+    public html: string = '<a href="https://google.com" target="_blank">GOOGLE</a>\n2ND line of HTML code!';
     public style: string = 'font-size: 15px; color: red;';
     public date: Date = new Date(new Date().getTime() - 123456789000);
 }
