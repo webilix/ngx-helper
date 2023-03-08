b4b7b62b23c55237595b3111849405a0ad89d851
Ali Amirnezhad <webilix@gmail.com>
2023-03-08 06:27:59 +0330

    Refactor exports

--- ngx-helper/src/public-api.ts
+++ ngx-helper/src/public-api.ts
@@ -2,45 +2,58 @@
  * Public API Surface of ngx-helper
  */
 
-export * from './lib/ngx-helper.component';
-export * from './lib/ngx-helper.module';
-export * from './lib/ngx-helper.service';
-export * from './lib/ngx-helper.values';
+export { NgxHelperComponent } from './lib/ngx-helper.component';
+export { NgxHelperModule } from './lib/ngx-helper.module';
+export { NgxHelperService } from './lib/ngx-helper.service';
+export { NGX_HELPER_LOADING_HEADER } from './lib/ngx-helper.values';
 
-export * from './lib/components/box/ngx-helper-box.component';
-export * from './lib/components/calendar/ngx-helper-calendar.component';
-export * from './lib/components/list/ngx-helper-list.component';
-export * from './lib/components/loading/ngx-helper-loading.component';
-export * from './lib/components/map/ngx-helper-map.component';
-export * from './lib/components/menu/ngx-helper-menu.component';
-export * from './lib/components/pagination/ngx-helper-pagination.component';
-export * from './lib/components/params/ngx-helper-params.component';
-export * from './lib/components/plate/ngx-helper-plate.component';
-export * from './lib/components/values/ngx-helper-values.component';
+export {
+    NgxHelperBoxComponent,
+    NgxHelperCalendarComponent,
+    NgxHelperListComponent,
+    NgxHelperLoadingComponent,
+    NgxHelperMapComponent,
+    NgxHelperMenuComponent,
+    NgxHelperPaginationComponent,
+    NgxHelperParamsComponent,
+    NgxHelperPlateComponent,
+    NgxHelperValuesComponent,
+} from './lib/components';
 
-export * from './lib/interfaces/ngx-helper-bottomsheet';
-export * from './lib/interfaces/ngx-helper-calendar';
-export * from './lib/interfaces/ngx-helper-dialog';
-export * from './lib/interfaces/ngx-helper-location';
-export * from './lib/interfaces/ngx-helper-upload';
+export {
+    INgxHelperBottomSheetConfig,
+    INgxHelperCalendarConfig,
+    INgxHelperCalendarPeriod,
+    INgxHelperCalendarValue,
+    NgxHelperCalendar,
+    INgxHelperDialogConfig,
+    INgxHelperLocation,
+    INgxHelperUpload,
+} from './lib/interfaces';
 
-export * from './lib/pipes/ngx-helper-bank-card.pipe';
-export * from './lib/pipes/ngx-helper-duration.pipe';
-export * from './lib/pipes/ngx-helper-file-size.pipe';
-export * from './lib/pipes/ngx-helper-jalali.pipe';
-export * from './lib/pipes/ngx-helper-mobile.pipe';
-export * from './lib/pipes/ngx-helper-multi-line.pipe';
-export * from './lib/pipes/ngx-helper-period.pipe';
-export * from './lib/pipes/ngx-helper-price.pipe';
-export * from './lib/pipes/ngx-helper-safe.pipe';
-export * from './lib/pipes/ngx-helper-value.pipe';
-export * from './lib/pipes/ngx-helper-weight.pipe';
+export {
+    NgxHelperBankCardPipe,
+    NgxHelperDurationPipe,
+    NgxHelperFileSizePipe,
+    NgxHelperJalaliPipe,
+    NgxHelperMobilePipe,
+    NgxHelperMultiLinePipe,
+    NgxHelperPeriodPipe,
+    NgxHelperPricePipe,
+    NgxHelperSafePipe,
+    NgxHelperValuePipe,
+    NgxHelperWeightPipe,
+} from './lib/pipes';
 
-export * from './lib/providers/ngx-helper-connection.service';
-export * from './lib/providers/ngx-helper-loading.service';
-export * from './lib/providers/ngx-helper-location.service';
+export { NgxHelperConnectionService, NgxHelperLoadingService, NgxHelperLocationService } from './lib/providers';
 
-export * from './lib/types/ngx-helper-list';
-export * from './lib/types/ngx-helper-menu';
-export * from './lib/types/ngx-helper-params';
-export * from './lib/types/ngx-helper-value';
+export {
+    NgxHelperListMenu,
+    NgxHelperMenu,
+    INgxHelperParamsOrder,
+    INgxHelperParamsUpdate,
+    INgxHelperParamsValue,
+    NgxHelperParams,
+    INgxHelperValues,
+    NgxHelperValue,
+} from './lib/types';
