92d4600e330a2cacc9ba64487d28badb11225204
Ali Amirnezhad <webilix@gmail.com>
2024-08-25 18:19:01 +0330

    Add description to INgxHelperValue

--- ngx-helper/value/src/ngx-helper-value.interface.ts
+++ ngx-helper/value/src/ngx-helper-value.interface.ts
@@ -3,6 +3,7 @@ import { NgxHelperValue } from './ngx-helper-value.type';
 export interface INgxHelperValue {
     title: string;
     value: NgxHelperValue;
+    description?: string;
     block?: boolean;
     copy?: boolean;
 }
